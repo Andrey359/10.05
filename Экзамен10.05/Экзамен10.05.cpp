9fc08f7b353c27f6523ca2206b7491296ec9c0cb
Андрей Третьяков <at.tehnol@outlook.com>
2023-05-12 15:44:27 +0300

    Исправлены ошибки

--- Экзамен10.05/Экзамен10.05.cpp
+++ Экзамен10.05/Экзамен10.05.cpp
@@ -1,51 +1,61 @@
 ﻿
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	srand(time(0));
 
-	bool replay; // повтор лучайных чисел
+	bool replay; // повтор случайных чисел
 	int range_Start, range_End; //диапазон
 	int size; // кол-во
 
 	cout << "Введите размер массива: "; cin >> size;
-	int* array = new int[size]; // создание массива
+	int* array = new int[size];
 
 	cout << "Введите даипазон случайных чисел" << endl;
 	cout << "От: "; cin >> range_Start;
 	cout << "До: "; cin >> range_End;
+	//Исправление ввод пользователя
+	if (range_Start > range_End) {
+		int l = range_Start;
+		range_Start = range_End;
+		range_End = l;
+	}
 
 	cout << "yas - 1, no - 0 " << endl;
 	cout << "Повторять числа: ";
 	cin >> replay;
-	if (!replay && (range_End - range_Start < size)) {
+	if (!replay && (range_End - range_Start < size - 1)) {
 		cerr << "Размер массива больше чем диапазон чисел!";
-		return 0;
+		return -1;
 	}
-
 	//генерация случайных чисел с повтором
 	if (replay) {
 		for (int i = 0; i < size; i++) {
 			array[i] = rand() % (range_End - range_Start + 1) + range_Start;
-			cout << array[i] << " ";
 		}
 	}
 	//генерация случайных чисел без повтора
 	else {
 		for (int i = 0; i < size; i++) {
 			array[i] = rand() % (range_End - range_Start + 1) + range_Start;
-			for (int j = 1; j < i; j++) {
-				if (array[j - 1] == array[j]) {
+			for (int j = 1; j <= i; j++) {
+				if (array[j - 1] == array[i]) {
 					i--;
 					break;
 				}
 			}
-			cout << array[i] << " ";
 		}
 	}
 
-    delete[] array; // удаление старого массива
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << " ";
+	}
+
+    delete[] array;
+	return 0;
 }
